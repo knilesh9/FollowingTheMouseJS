73d0d31c424af874dd16a4fa1a4901cbca33e634
itsmek <yyooggeesshh0011@gmail.com>
2023-12-02 20:36:00 +0530

    Just basic working messy

---
+++ script.js
@@ -0,0 +1,18 @@
+var rect = document.querySelector(".rectBox");
+
+window.addEventListener("mousemove", function(details){
+    //In this case, "details" is an arbitrary name for the event object; you could use any valid identifier
+    // The event object is automatically passed to the function when the event occurs
+
+
+    console.log("Mouse moved");
+
+    var valX = gsap.utils.mapRange(0, window.innerWidth,
+                         200, window.innerWidth - 200, 
+                         details.clientX);
+
+    gsap.to(".rectBox", {
+        left: valX,
+    });
+
+});
